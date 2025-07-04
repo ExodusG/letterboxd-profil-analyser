148cae081b1383dbf6eb7e4011c0156a0e6c8d80
Quentin Le Lan <quentin.lelan@socleo.com>
2025-07-04 16:17:47 +0200

    init

---
+++ Account_analysis.py
@@ -0,0 +1,279 @@
+import streamlit as st
+from src.solo import *
+import zipfile
+import tempfile
+import gspread
+from google.oauth2 import service_account
+import sentry_sdk
+import math
+import json
+
+st.set_page_config(
+    page_title="Letterboxd analysis",
+    page_icon="🎥",
+    layout="wide",
+    menu_items={
+        'Report a bug': "mailto:lelan.quentin56@gmail.com",
+    }
+)
+
+st.markdown("""
+# 🎬 Welcome to our **Letterboxd Profile Analyzer**!
+
+With just a few clicks, you'll discover your movie habits and get interesting insights about your film preferences.  
+
+🚀 This is just the first version of the project — many improvements are planned, including a feature to compare two profiles!
+
+---
+
+## 📥 How to get started:
+
+1. **Log into** your Letterboxd account (on pc)
+2. Go to **Settings → Data → Export Your Data**
+3. **Download** the data file
+4. **Upload** the zipfile below and enjoy your personalized analysis!
+
+---
+""")
+#st.page_link("pages/Compare.py", label="Compare", icon="1️⃣")
+sentry_sdk.init(
+    dsn= st.secrets["dns"],
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
+
+with st.spinner("Set-up...", show_time=False):
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+    )
+
+    client = gspread.authorize(credentials)
+    spreadsheet = client.open("all_movies_data")
+    sheet = spreadsheet.sheet1
+
+    # Charger les données existantes depuis Google Sheet
+    data = sheet.get_all_records()
+
+    error_sheet = spreadsheet.worksheet("error")
+temp_dir = tempfile.TemporaryDirectory()
+temp_name=temp_dir.name
+
+api_key = st.secrets['API_KEY']
+base_url = 'http://www.omdbapi.com/'
+
+if "uploader_key" not in st.session_state:
+    st.session_state["uploader_key"] = 1
+
+uploaded_files = st.file_uploader(
+    "Choose your zip file", accept_multiple_files=False, key=st.session_state["uploader_key"]
+)
+
+def sanitize(value):
+    if isinstance(value, float):
+        if math.isnan(value) or math.isinf(value):
+            return ''
+    return value
+
+def getMovie(all_movies, dfF, sheet):
+    df_errors = pd.DataFrame(columns=["filename", "error", *dfF.columns])
+    progress_text = "Get movie data. Please wait."
+    my_bar = st.progress(0, text=progress_text)
+    df_movie = pd.DataFrame()
+
+    existing_movies = set(
+        zip(all_movies['Title'].dropna().astype(str), all_movies['Year'].dropna())
+    )
+    i = 0
+
+    # Itérer sur les lignes de dfF pour comparer (Name, Year)
+    for _, row in dfF.iterrows():
+        title_year = (row['Name'], row['Year'])
+        if title_year not in existing_movies:
+            try:
+                movie_data = get_movie_data_by_title(row['Name'], row['Year'], base_url, api_key) 
+                #print(movie_data)
+                if movie_data.get('Error') is not None:
+                    #sentry_sdk.capture_message(f"Movie not found: {row.to_dict()}")
+                    #print(row)
+                    df_errors.loc[len(df_errors)] = [uploaded_files.name, movie_data['Error'], *row.values]
+                else:
+                    df_movie = pd.concat([df_movie, pd.DataFrame([movie_data])], ignore_index=True)
+            except Exception as e:
+                #sentry_sdk.capture_message(f"Movie not found: {row.to_dict()}")
+                #print(row)
+                df_errors.loc[len(df_errors)] = [uploaded_files.name, str(e), *row.values]
+        my_bar.progress(int(100 * i / len(dfF)), text=progress_text)
+        i += 1
+
+    if not df_errors.empty:
+        cleaned_rows = df_errors.applymap(sanitize).values.tolist()
+        error_sheet.append_rows(cleaned_rows)
+
+    if len(df_movie) != 0:
+        df_movie['Ratings'] = df_movie['Ratings'].apply(lambda x: json.dumps(x) if isinstance(x, list) else x)
+        df_movie = clean_year(df_movie)
+
+        rows = df_movie.applymap(sanitize).values.tolist()
+        sheet.append_rows(rows)
+
+        all_movies = pd.concat([all_movies, df_movie]).drop_duplicates(subset=['Title', 'Year'])
+
+        # peut ajouter chaque nouvelle ligne, plutot que tout effacer
+        #sheet.clear()  # Efface l'ancienne feuille
+        #sheet.update([all_movies.columns.values.tolist()] + all_movies.values.tolist())
+
+    my_bar.empty()
+    return all_movies
+
+def general_info():
+    runtime=computeRuntime(watch_df)
+    runtimeW =computeRuntime(watchlist_df)
+    st.title('Global info')
+    col,col2,col3 = st.columns(3)
+    with col:
+        st.markdown(f"<h1><span style='color:#83c9ff'>{round(runtime)}</span> hours watched</h1>", unsafe_allow_html=True)
+    with col2:
+        st.markdown(f"<h1><span style='color:#83c9ff'>{len(watch_df.index)}</span> movies watched</h1>", unsafe_allow_html=True)
+    with col3:
+        st.markdown(f"<h1><span style='color:#83c9ff'>{len(watch_df['Director'].unique())}</span> directors</h1>", unsafe_allow_html=True)
+    col4,col5 = st.columns(2)
+    with col4:
+        st.markdown(f"<h1><span style='color:#83c9ff'>{round(runtimeW)}</span> hours to watch</h1>", unsafe_allow_html=True)
+    with col5:
+        st.markdown(f"<h1><span style='color:#83c9ff'>{len(watchlist_df.index)}</span> movies to watch</h1>", unsafe_allow_html=True)
+
+st.markdown(
+    """
+    <style>
+    div[data-testid="stTabs"]{
+        padding-top:30px;
+    }
+    /* Sélectionner les titres des tabs */
+    div[data-baseweb="tab-list"] > button[data-baseweb="tab"] > div[data-testid="stMarkdownContainer"] {
+        font-size: 30px; /* Changer la taille ici */
+        min-width:200px;
+    }
+
+    @media(max-width:610px) {
+        div[data-baseweb="tab-list"] > button[data-baseweb="tab"] > div[data-testid="stMarkdownContainer"] {
+        font-size: inherit; /* Changer la taille ici */
+        min-width:inherit;
+        }
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+if uploaded_files is not None:
+    try:
+        with zipfile.ZipFile(uploaded_files, 'r') as zip_ref:
+            zip_ref.extractall(temp_name)
+        
+        watchlist=pd.read_csv(temp_name+'/watchlist.csv')
+        watched=pd.read_csv(temp_name+'/watched.csv')
+        rating=pd.read_csv(temp_name+'/ratings.csv')
+
+
+        watchlist = clean_year(watchlist)
+        watched=clean_year(watched)
+        rating=clean_year(rating)
+
+        #all_movies=pd.read_csv('./data/all_movies_data.csv')
+        all_movies = pd.DataFrame(data)
+        all_movies=clean_year(all_movies)
+        dfF=pd.concat([watched,watchlist])
+
+        all_movies = getMovie(all_movies, dfF, sheet)
+
+        watch_df = pd.merge(watched, all_movies, how='inner', left_on=["Name", "Year"], right_on=["Title", "Year"]).drop_duplicates()
+        watchlist_df=pd.merge(watchlist, all_movies, how='inner', left_on=["Name", "Year"], right_on=["Title", "Year"]).drop_duplicates()
+
+        rating_df=pd.merge(rating, all_movies, how='inner', left_on=["Name", "Year"], right_on=["Title", "Year"]).drop_duplicates()
+        rating_df['Rating']=rating_df['Rating']*2
+        rating_df=clean_imdbr(rating_df)
+        rating_df['diff_rating']=rating_df['Rating']-rating_df['imdbRating']
+
+        q1,q2,q3=compute_quartile(all_movies)
+
+        ### DEBUT INTERFACE
+        general_info()
+        #movie_per_year(watch_df)
+
+        tab1, tab2, tab3 = st.tabs(["Watched", "Watchlist","Rating"])
+        #WATCHED
+        with tab1:
+            st.header("Some stats on the movies you've seen", divider='blue')
+            genre(watch_df)
+            watched_actor_col, watched_director_col = st.columns(2)
+            bottom,top=famous(watch_df)
+            with watched_actor_col:
+                fig=actor(watch_df)
+                st.plotly_chart(fig)
+            with watched_director_col:
+                fig=director(watch_df)
+                st.plotly_chart(fig)
+            st.header("Lesser-known and better-known films by number of IMDB votes", divider='blue')
+            most_watched_col, less_watched_col = st.columns(2)
+            with less_watched_col:
+                st.write("Lesser-known movie watched")
+                st.write(bottom)
+            with most_watched_col:
+                st.write("Better-known movie watched")
+                st.write(top)
+            cinephile(watch_df,q1,q2,q3,"a")
+            decade(watch_df)
+            mapW(watch_df)
+        #WATCHLIST
+        with tab2:
+            st.header("Some stats on the films you want to see", divider='blue')
+            genre(watchlist_df)
+            watched_actor_col, watched_director_col = st.columns(2)
+            bottom,top=famous(watchlist_df)
+            with watched_actor_col:
+                fig=actor(watchlist_df)
+                st.plotly_chart(fig)
+            with watched_director_col:
+                fig=director(watchlist_df)
+                st.plotly_chart(fig)
+            st.header("Lesser-known and better-known films by number of IMDB votes", divider='blue')
+            most_watched_col, less_watched_col = st.columns(2)
+            with less_watched_col:
+                st.write("Lesser-known movie in your watchlist")
+                st.write(bottom)
+            with most_watched_col:
+                st.write("Better-known movie in your watchlist")
+                st.write(top)
+            cinephile(watchlist_df,q1,q2,q3,"b")
+            decade(watchlist_df)
+            mapW(watchlist_df)
+        #RATING
+        with tab3:
+            st.header("Some stats on your ratings", divider='blue')
+            sur_note,sous_note=diff_rating(rating_df)
+            st.write('The movies you rated most compared to IMDB')
+            st.write(sur_note)
+            st.write('The movies you most underrated compared to IMDB')
+            st.write(sous_note)
+            rating_director(rating_df,watch_df)
+            rating_actor(rating_df)
+            genre_rating(rating_df)
+            comparaison_rating(rating_df)
+
+    except zipfile.BadZipFile:
+        st.session_state["uploader_key"] += 1
+        #trick to remove bad file
+        st.warning('This is not a zipfile', icon="⚠️")
+        st.rerun()
+    except FileNotFoundError:
+        st.write(f"File not found.")
+temp_dir.cleanup()
+
+
+#st.sidebar.title("About")
+st.info(
+    """
+    This app is maintained by [Exodus](https://exodusg.github.io/) and [Montro](https://github.com/Montr0-0). 
+"""
+)
---
+++ src/solo.py
@@ -0,0 +1,326 @@
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import requests
+import plotly.graph_objects as go
+import pycountry
+import streamlit as st
+
+
+def computeRuntime(df):
+    df['Runtime']=df['Runtime'].replace('nan', np.nan)
+    df = df.dropna(subset=['Runtime'])
+    df['Runtime'] = df['Runtime'].astype(str).str.replace(r'[Ss]', '5', regex=True)
+
+    runtimeW=df['Runtime'].str.split(' ').str[0]
+    runtimeW = runtimeW.apply(lambda x: int(x) if x not in ['', 'N/A'] else None)
+    runtimeW=runtimeW.sum() / 60
+    return runtimeW
+
+def get_movie_data_by_title(title,year,base_url,api_key):
+    response = requests.get(base_url, params={'apikey': api_key, 't': title, 'y':year})
+    return response.json()
+
+def compute_quartile(df):
+    quartile=df.copy()
+    quartile['imdbVotes'] = quartile['imdbVotes'].replace("N/A", np.nan)
+    quartile = quartile.dropna(subset=['imdbVotes'])
+    #quartile['imdbVotes'] = quartile['imdbVotes'].str.replace(',', '')
+    quartile['imdbVotes'] = quartile['imdbVotes'].astype(str).str.replace(',', '')
+    quartile['imdbVotes'] = quartile['imdbVotes'].apply(
+        lambda x: float(x) if x not in ['', 'N/A'] else np.nan
+    )
+    #quartile['imdbVotes'] = quartile['imdbVotes'].apply(lambda x: float(x) if x != '' else np.nan)
+    quartile = quartile.dropna(subset=['imdbVotes'])
+    quartile=quartile.sort_values(by=['imdbVotes'])
+
+    q1=np.quantile(quartile['imdbVotes'], 0.25)
+    q2=np.quantile(quartile['imdbVotes'], 0.50)
+    q3=np.quantile(quartile['imdbVotes'], 0.75)
+    return q1,q2,q3
+
+def clean_year(df):
+    df = df[pd.to_numeric(df['Year'], errors='coerce').notna()]
+    df['Year'] = df['Year'].astype(float).astype(int).astype(str)
+    return df
+def clean_imdbv(df):
+    df['imdbVotes'] = df['imdbVotes'].replace("N/A", np.nan)
+    df = df.dropna(subset=['imdbVotes'])
+    df['imdbVotes'] = df['imdbVotes'].str.replace(',', '').astype(float)
+    return df
+def clean_imdbr(df):
+    df['imdbRating'] = df['imdbRating'].replace("N/A", np.nan)
+    df = df.dropna(subset=['imdbRating'])
+    df['imdbRating'] = df['imdbRating'].apply(lambda x: float(x) if x != '' else None)
+    return df
+
+def movie_per_year(df):
+    films_par_annee = df['Year'].value_counts().sort_index()
+
+    df_par_annee = films_par_annee.reset_index()
+    df_par_annee.columns = ['Year', 'Number of Movies']
+
+    fig = px.line(df_par_annee, x='Year', y='Number of Movies', title='Number of movie per year')
+
+    st.plotly_chart(fig)
+
+def genre(df):
+    df_genres = df['Genre'].dropna().str.split(', ')
+
+    df_genres_exploded = df_genres.explode()
+    genre_counts = df_genres_exploded.value_counts().reset_index()
+    genre_counts.columns = ['Genre', 'Number of movie']
+    fig = px.bar(genre_counts, x='Genre', y='Number of movie', 
+                title='Number of films by genre', 
+                text='Number of movie')
+
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', showlegend=False)
+    st.plotly_chart(fig)
+
+def director(df):
+    # possibilité d'avoir plusieurs réalisateurs, prend le premier ou comtpe les deux séparément ?
+    nb_real = df['Director'].dropna().str.split(', ').explode().value_counts().head(25).reset_index()
+    nb_real.columns = ['Director', 'Number of Movies']
+
+    fig = px.bar(nb_real, x='Director', y='Number of Movies',
+                title='Number of films by director',
+                text='Number of Movies')
+    return fig
+
+def actor(df):
+    nb_actor = df['Actors'].dropna().str.split(', ').explode().value_counts().head(25).reset_index()
+    nb_actor.columns = ['Actors', 'Number of Movies']
+
+    fig = px.bar(nb_actor, x='Actors', y='Number of Movies',
+                title='Number of films by actor',
+                text='Number of Movies')
+    return fig
+
+def famous(df):
+    movies_rating=df.copy()
+    movies_rating['imdbVotes'] = df['imdbVotes'].replace("N/A", np.nan)
+    movies_rating['imdbVotes'] = movies_rating['imdbVotes'].replace("", np.nan)
+    movies_rating = movies_rating.dropna(subset=['imdbVotes'])
+    movies_rating['imdbVotes'] = movies_rating['imdbVotes'].astype(str).str.replace(',', '')
+    movies_rating['imdbVotes']=movies_rating['imdbVotes'].astype(float)
+    bottom_5_votes = movies_rating.sort_values(by='imdbVotes', ascending=True).head(10)
+    bottom=bottom_5_votes[['Title', 'imdbVotes']].reset_index(drop=True)
+
+    top_5_votes = movies_rating.sort_values(by='imdbVotes', ascending=False).head(10)
+    top=top_5_votes[['Title', 'imdbVotes']].reset_index(drop=True)
+    return bottom,top
+
+def cinephile(df,q1,q2,q3,key):
+    habit=df.copy()
+    habit['imdbVotes'] = habit['imdbVotes'].replace("N/A", np.nan)
+    habit['imdbVotes'] = habit['imdbVotes'].replace("", np.nan)
+    habit = habit.dropna(subset=['imdbVotes'])
+    #habit['imdbVotes'] = habit['imdbVotes'].str.replace(',', '').astype(float)
+    habit['imdbVotes'] = habit['imdbVotes'].apply(
+    lambda x: float(x.replace(',', '')) if isinstance(x, str) else x
+    )
+
+    # Définir les bornes des intervalles
+    intervals = [
+        (habit['imdbVotes'] <= q1),             # Intervalle 0 <= Q1
+        (habit['imdbVotes'] > q1) & (habit['imdbVotes'] <= q2),  # Intervalle Q1 < Q2
+        (habit['imdbVotes'] > q2) & (habit['imdbVotes'] <= q3),  # Intervalle Q2 < Q3
+        (habit['imdbVotes'] > q3)               # Intervalle Q3 < inf
+    ]
+
+    # Créer un tableau avec les comptages pour chaque intervalle
+    counts = [
+        len(habit[intervals[0]]), 
+        len(habit[intervals[1]]), 
+        len(habit[intervals[2]]), 
+        len(habit[intervals[3]])
+    ]
+    result = pd.DataFrame({
+        'category': [' Obscure', 'Lesser-known', 'Well-known', 'Mainstream'],
+        'number': counts
+    })
+    fig=px.bar(result,x='category',y='number',title="Popularity of films seen",text='number')
+    st.plotly_chart(fig,key=key)
+
+def decade(df):
+    years = df['Year'].dropna().explode()
+    years = years.astype(int)
+
+    years = pd.cut(years, bins=[1900, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020, 2025], 
+                labels=['1900-1950', '1951-1960', '1961-1970', '1971-1980', '1981-1990', 
+                        '1991-2000', '2001-2010', '2011-2020', '2021-2025'])
+    years = years.value_counts().reset_index()
+    years.columns = ['Year', 'Number of Movies']
+
+    years = years.sort_values(by='Year')
+
+    fig=px.bar(years, x='Year', y='Number of Movies', title='Films by decade',text='Number of Movies')
+    st.plotly_chart(fig)
+
+def mapW(df):
+    df['Country'] = df['Country'].dropna().str.split(', ')
+
+    df_genres_exploded = df.explode('Country')
+    genre_counts = df_genres_exploded['Country'].value_counts().reset_index()
+    countries = {country.name: country.alpha_3 for country in pycountry.countries}
+
+    genre_counts['code'] = genre_counts['Country'].map(countries)
+
+    fig = px.choropleth(
+        genre_counts,
+        locations="code",
+        color="count",
+        hover_name="Country",
+        color_continuous_scale=px.colors.sequential.Blues,
+        projection="natural earth"
+    )
+
+    fig.update_geos(
+        showcoastlines=False,
+        showland=True,
+        landcolor="#0E1117",
+        showocean=True,
+        oceancolor="#0E1117",
+        showframe=False,
+        showcountries=True,
+        countrycolor="gray"
+    )
+
+    fig.update_layout(
+        title_text="Breakdown of films by country",
+        title_font_color="white",
+        geo_bgcolor="#0E1117",
+        plot_bgcolor="#0E1117",
+        paper_bgcolor="#0E1117",
+        font=dict(color="white"),
+        margin=dict(l=0, r=0, t=40, b=0)
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+def diff_rating(df):
+    sur_note=df.sort_values(by=['diff_rating'],ascending=False)
+    sur_note=sur_note.head(10).reset_index(drop=True)
+
+    sous_note=df.sort_values(by=['diff_rating'])
+    sous_note=sous_note.head(10).reset_index(drop=True)
+    return sur_note[['Name','Rating','imdbRating','diff_rating']],sous_note[['Name','Rating','imdbRating','diff_rating']]
+
+def rating_director(rating_df,watch_df):
+    nb_real= watch_df['Director'].value_counts().head(25)
+    df_top_directors = rating_df[rating_df['Director'].isin(nb_real.index)]
+    nb_films_par_director = df_top_directors['Director'].value_counts().reset_index()
+    nb_films_par_director.columns = ['Director', 'Number of movie']
+    mean_rating = df_top_directors.groupby('Director')['Rating'].mean().rename('Moyenne_Rating').reset_index()
+    mean_rating.columns = ['Director', 'Moyenne_Rating']
+
+
+    df_plot = pd.merge(nb_films_par_director, mean_rating, on='Director')
+
+    fig = px.bar(
+        nb_films_par_director,
+        x='Director',
+        y='Number of movie',
+        title='Top 25 most viewed directors and their average rating',
+        labels={'Director': 'Director', 'Number of movie': 'Number of movie'},
+        text='Number of movie'
+    )
+    fig.add_scatter(x=df_plot['Director'],y=df_plot['Moyenne_Rating'], name='Mean Rating Letterboxd',mode='lines+markers')
+    fig.update_layout(xaxis_tickangle=-45)
+    st.plotly_chart(fig)
+
+def rating_actor(rating_df):
+    nb_actor = rating_df['Actors'].dropna().str.split(', ').explode().value_counts().head(25)
+    top_actors = nb_actor.index
+
+    df_exploded = rating_df.dropna(subset=['Actors']).copy()
+    df_exploded['Actor'] = df_exploded['Actors'].str.split(', ')
+    df_exploded = df_exploded.explode('Actor')
+
+    df_top_actors = df_exploded[df_exploded['Actor'].isin(top_actors)]
+
+    nb_films_par_actor = df_top_actors['Actor'].value_counts().reset_index()
+    nb_films_par_actor.columns = ['Actor', 'Nb_Films']
+
+    mean_rating_actor = df_top_actors.groupby('Actor')['Rating'].mean().reset_index()
+    mean_rating_actor.columns = ['Actor', 'Moyenne_Rating']
+
+    df_actor_plot = pd.merge(nb_films_par_actor, mean_rating_actor, on='Actor')
+
+    fig = px.bar(
+        df_actor_plot,
+        x='Actor',
+        y='Nb_Films',
+        title='Top 25 most viewed actors and their average rating',
+        labels={'Director': 'Director', 'Nb_Films': 'Number of movie'},
+        text='Nb_Films'
+    )
+    fig.add_scatter(x=df_actor_plot['Actor'],y=df_actor_plot['Moyenne_Rating'], name='Mean Rating Letterboxd',mode='lines+markers')
+    fig.update_layout(xaxis_tickangle=-45)
+    st.plotly_chart(fig)
+
+def genre_rating(rating_df):
+    rating_df['Genre'] = rating_df['Genre'].dropna().str.split(', ')
+    df_genres_exploded = rating_df.explode('Genre')
+    genre_counts = df_genres_exploded['Genre'].value_counts().reset_index()
+    genre_counts.columns = ['Genre', 'Nombre de films']
+
+    mean_rating_genre = df_genres_exploded.groupby('Genre')['Rating'].mean().reset_index()
+    mean_rating_genre.columns = ['Genre', 'Moyenne_Rating']
+
+    df_genre_plot = pd.merge(genre_counts, mean_rating_genre, on='Genre')
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x=df_genre_plot['Genre'],
+        y=df_genre_plot['Nombre de films'],
+        name='Number of movie',
+        yaxis='y1',
+        text=df_genre_plot['Nombre de films']
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=df_genre_plot['Genre'],
+        y=df_genre_plot['Moyenne_Rating'],
+        name='Mean rating',
+        yaxis='y2',
+        mode='lines+markers'
+    ))
+
+    fig.update_layout(
+        title='Number of films and average rating by genre',
+        xaxis=dict(title='Genre', tickangle=-45),
+        yaxis=dict(
+            title='Number of movie',
+            side='left'
+        ),
+        yaxis2=dict(
+            title='Mean rating',
+            overlaying='y',
+            side='right',
+            showgrid=False,
+            range=[df_genre_plot['Moyenne_Rating'].min() - 0.5, df_genre_plot['Moyenne_Rating'].max() + 0.5]  # échelle personnalisée ici
+        ),
+        legend=dict(x=1.02, y=0.99),
+        height=500
+    )
+    st.plotly_chart(fig)
+
+def comparaison_rating(rating_df):
+     # Groupby pour compter les valeurs dans 'Rating'
+    rate=rating_df.copy()
+    rate['imdbRating'] = pd.to_numeric(rate['imdbRating'], errors='coerce')
+    rate=rate.dropna(subset=['imdbRating'])
+    rate['imdbRating']=(rate['imdbRating'] * 2).round() / 2
+    rate = rate.groupby('imdbRating').size().reset_index(name='Number of movie')
+
+    fig = px.bar(rate, x='imdbRating', y='Number of movie', title="Breakdown of IMDB ratings and your own ratings",labels={'imdbRating': 'Rating', 'Number of movie': 'Number of movie'},text='Number of movie',)
+    fig.data[0].name = "Rating IMDB"
+
+    letterboxd_rate = rating_df.groupby('Rating').size().reset_index(name='Number of movie')
+    
+    fig.add_bar(x=letterboxd_rate['Rating'], y=letterboxd_rate['Number of movie'], name='Rating Letterboxd',text=letterboxd_rate['Number of movie'])
+    fig.update_layout(barmode='group', showlegend=True)
+
+    st.plotly_chart(fig)
