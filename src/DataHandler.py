459a6c990dab1cb5fe62b6506875acee3edd8307
exodus <exodus@dev.myguest.virtualbox.org>
2025-07-27 22:04:44 +0200

    fix import

--- src/DataHandler.py
+++ src/DataHandler.py
@@ -4,6 +4,8 @@ import pandas as pd
 import tempfile
 from datetime import datetime
 import pycountry
+from wordcloud import WordCloud
+from stop_words import get_stop_words
 
 # modules internes
 import src.ApiHandler as api_handler
